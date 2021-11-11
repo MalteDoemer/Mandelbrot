5275a87889da3f549ecb69b98daa0c0053b93599
Malte Dömer <maltedoemer@gmail.com>
2021-11-11 16:27:21 +0100

    pulled in the headers and updated gitignore

---
+++ inc/GLFWApplication.hpp
@@ -0,0 +1,323 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+namespace mygl {
+
+using namespace glm;
+
+enum class Key : int {
+    KeySpace = GLFW_KEY_SPACE,
+    KeyApostrophe = GLFW_KEY_APOSTROPHE,
+    KeyComma = GLFW_KEY_COMMA,
+    KeyMinus = GLFW_KEY_MINUS,
+    KeyPeriod = GLFW_KEY_PERIOD,
+    KeySlash = GLFW_KEY_SLASH,
+    Key0 = GLFW_KEY_0,
+    Key1 = GLFW_KEY_1,
+    Key2 = GLFW_KEY_2,
+    Key3 = GLFW_KEY_3,
+    Key4 = GLFW_KEY_4,
+    Key5 = GLFW_KEY_5,
+    Key6 = GLFW_KEY_6,
+    Key7 = GLFW_KEY_7,
+    Key8 = GLFW_KEY_8,
+    Key9 = GLFW_KEY_9,
+    KeySemicolon = GLFW_KEY_SEMICOLON,
+    KeyEqual = GLFW_KEY_EQUAL,
+    KeyA = GLFW_KEY_A,
+    KeyB = GLFW_KEY_B,
+    KeyC = GLFW_KEY_C,
+    KeyD = GLFW_KEY_D,
+    KeyE = GLFW_KEY_E,
+    KeyF = GLFW_KEY_F,
+    KeyG = GLFW_KEY_G,
+    KeyH = GLFW_KEY_H,
+    KeyI = GLFW_KEY_I,
+    KeyJ = GLFW_KEY_J,
+    KeyK = GLFW_KEY_K,
+    KeyL = GLFW_KEY_L,
+    KeyM = GLFW_KEY_M,
+    KeyN = GLFW_KEY_N,
+    KeyO = GLFW_KEY_O,
+    KeyP = GLFW_KEY_P,
+    KeyQ = GLFW_KEY_Q,
+    KeyR = GLFW_KEY_R,
+    KeyS = GLFW_KEY_S,
+    KeyT = GLFW_KEY_T,
+    KeyU = GLFW_KEY_U,
+    KeyV = GLFW_KEY_V,
+    KeyW = GLFW_KEY_W,
+    KeyX = GLFW_KEY_X,
+    KeyY = GLFW_KEY_Y,
+    KeyZ = GLFW_KEY_Z,
+    KeyLeftBracket = GLFW_KEY_LEFT_BRACKET,
+    KeyBackslash = GLFW_KEY_BACKSLASH,
+    KeyRightBracket = GLFW_KEY_RIGHT_BRACKET,
+    KeyGraveAccent = GLFW_KEY_GRAVE_ACCENT,
+    KeyWorld1 = GLFW_KEY_WORLD_1,
+    KeyWorld2 = GLFW_KEY_WORLD_2,
+
+    KeyEscape = GLFW_KEY_ESCAPE,
+    KeyEnter = GLFW_KEY_ENTER,
+    KeyTab = GLFW_KEY_TAB,
+    KeyBackspace = GLFW_KEY_BACKSPACE,
+    KeyInsert = GLFW_KEY_INSERT,
+    KeyDelete = GLFW_KEY_DELETE,
+    KeyRight = GLFW_KEY_RIGHT,
+    KeyLeft = GLFW_KEY_LEFT,
+    KeyDown = GLFW_KEY_DOWN,
+    KeyUp = GLFW_KEY_UP,
+    KeyPage_up = GLFW_KEY_PAGE_UP,
+    KeyPage_down = GLFW_KEY_PAGE_DOWN,
+    KeyHome = GLFW_KEY_HOME,
+    KeyEnd = GLFW_KEY_END,
+    KeyCaps_lock = GLFW_KEY_CAPS_LOCK,
+    KeyScroll_lock = GLFW_KEY_SCROLL_LOCK,
+    KeyNum_lock = GLFW_KEY_NUM_LOCK,
+    KeyPrint_screen = GLFW_KEY_PRINT_SCREEN,
+    KeyPause = GLFW_KEY_PAUSE,
+    KeyF1 = GLFW_KEY_F1,
+    KeyF2 = GLFW_KEY_F2,
+    KeyF3 = GLFW_KEY_F3,
+    KeyF4 = GLFW_KEY_F4,
+    KeyF5 = GLFW_KEY_F5,
+    KeyF6 = GLFW_KEY_F6,
+    KeyF7 = GLFW_KEY_F7,
+    KeyF8 = GLFW_KEY_F8,
+    KeyF9 = GLFW_KEY_F9,
+    KeyF10 = GLFW_KEY_F10,
+    KeyF11 = GLFW_KEY_F11,
+    KeyF12 = GLFW_KEY_F12,
+    KeyF13 = GLFW_KEY_F13,
+    KeyF14 = GLFW_KEY_F14,
+    KeyF15 = GLFW_KEY_F15,
+    KeyF16 = GLFW_KEY_F16,
+    KeyF17 = GLFW_KEY_F17,
+    KeyF18 = GLFW_KEY_F18,
+    KeyF19 = GLFW_KEY_F19,
+    KeyF20 = GLFW_KEY_F20,
+    KeyF21 = GLFW_KEY_F21,
+    KeyF22 = GLFW_KEY_F22,
+    KeyF23 = GLFW_KEY_F23,
+    KeyF24 = GLFW_KEY_F24,
+    KeyF25 = GLFW_KEY_F25,
+    KeyKp0 = GLFW_KEY_KP_0,
+    KeyKp1 = GLFW_KEY_KP_1,
+    KeyKp2 = GLFW_KEY_KP_2,
+    KeyKp3 = GLFW_KEY_KP_3,
+    KeyKp4 = GLFW_KEY_KP_4,
+    KeyKp5 = GLFW_KEY_KP_5,
+    KeyKp6 = GLFW_KEY_KP_6,
+    KeyKp7 = GLFW_KEY_KP_7,
+    KeyKp8 = GLFW_KEY_KP_8,
+    KeyKp9 = GLFW_KEY_KP_9,
+    KeyKpDecimal = GLFW_KEY_KP_DECIMAL,
+    KeyKpDivide = GLFW_KEY_KP_DIVIDE,
+    KeyKpMultiply = GLFW_KEY_KP_MULTIPLY,
+    KeyKpSubtract = GLFW_KEY_KP_SUBTRACT,
+    KeyKpAdd = GLFW_KEY_KP_ADD,
+    KeyKpEnter = GLFW_KEY_KP_ENTER,
+    KeyKpEqual = GLFW_KEY_KP_EQUAL,
+    KeyLeftShift = GLFW_KEY_LEFT_SHIFT,
+    KeyLeftControl = GLFW_KEY_LEFT_CONTROL,
+    KeyLeftAlt = GLFW_KEY_LEFT_ALT,
+    KeyLeftSuper = GLFW_KEY_LEFT_SUPER,
+    KeyRightShift = GLFW_KEY_RIGHT_SHIFT,
+    KeyRightControl = GLFW_KEY_RIGHT_CONTROL,
+    KeyRightAlt = GLFW_KEY_RIGHT_ALT,
+    KeyRightSuper = GLFW_KEY_RIGHT_SUPER,
+    KeyMenu = GLFW_KEY_MENU,
+
+};
+
+enum class KeyAction : int {
+    Release = GLFW_RELEASE,
+    Press = GLFW_PRESS,
+    Repeat = GLFW_REPEAT,
+};
+
+enum Modifier : int {
+    MOD_ALT = GLFW_MOD_ALT,
+    MOD_CONTROL = GLFW_MOD_CONTROL,
+    MOD_SHIFT = GLFW_MOD_SHIFT,
+    MOD_SUPER = GLFW_MOD_SUPER,
+    MOD_CAPS_LOCK = GLFW_MOD_CAPS_LOCK,
+    MOD_NUM_LOCK = GLFW_MOD_NUM_LOCK,
+};
+
+struct KeyEvent {
+    Key key;
+    KeyAction action;
+    Modifier modifiers;
+};
+
+enum class MouseButton : int {
+    MouseButton1 = GLFW_MOUSE_BUTTON_1,
+    MouseButton2 = GLFW_MOUSE_BUTTON_2,
+    MouseButton3 = GLFW_MOUSE_BUTTON_3,
+    MouseButton4 = GLFW_MOUSE_BUTTON_4,
+    MouseButton5 = GLFW_MOUSE_BUTTON_5,
+    MouseButton6 = GLFW_MOUSE_BUTTON_6,
+    MouseButton7 = GLFW_MOUSE_BUTTON_7,
+    MouseButton8 = GLFW_MOUSE_BUTTON_8,
+    MouseButtonLast = GLFW_MOUSE_BUTTON_LAST,
+    MouseButtonLeft = GLFW_MOUSE_BUTTON_LEFT,
+    MouseButtonRight = GLFW_MOUSE_BUTTON_RIGHT,
+    MouseButtonMiddle = GLFW_MOUSE_BUTTON_MIDDLE,
+};
+
+enum class MouseAction : int {
+    Release = GLFW_RELEASE,
+    Press = GLFW_PRESS,
+    // Repeat = GLFW_REPEAT,
+};
+
+struct MouseEvent {
+    MouseButton button;
+    MouseAction action;
+    Modifier modifiers;
+};
+
+class GLFWApplication {
+public:
+    GLFWApplication() = default;
+
+    bool init(int width, int height)
+    {
+        glfwSetErrorCallback(glfw_error_callback);
+
+        if (!glfwInit()) {
+            return false;
+        }
+
+        if (!pre_init()) {
+            return false;
+        }
+
+        m_window = glfwCreateWindow(width, height, m_name.c_str(), NULL, NULL);
+
+        if (!m_window) {
+            return false;
+        }
+
+        glfwMakeContextCurrent(m_window);
+
+        glfwSetWindowUserPointer(m_window, this);
+
+        glfwSetKeyCallback(m_window, glfw_key_callback);
+        glfwSetScrollCallback(m_window, glfw_scroll_callback);
+        glfwSetCursorPosCallback(m_window, glfw_cursor_callback);
+        glfwSetWindowSizeCallback(m_window, glfw_window_size_callback);
+        glfwSetMouseButtonCallback(m_window, glfw_mouse_button_callback);
+        // glfwSetCursorEnterCallback();
+        // glfwSetWindowCloseCallback();
+
+        if (glewInit() != GLEW_OK) {
+            return false;
+        }
+
+        glEnable(GL_DEBUG_OUTPUT);
+        glDebugMessageCallback(gl_error_callback, this);
+
+        return post_init();
+    }
+
+    ~GLFWApplication() { glfwTerminate(); }
+
+    ivec2 window_size()
+    {
+        ivec2 size;
+        glfwGetWindowSize(m_window, &size.x, &size.y);
+        return size;
+    }
+
+    dvec2 mouse_pos()
+    {
+        dvec2 mouse;
+        glfwGetCursorPos(m_window, &mouse.x, &mouse.y);
+        return mouse;
+    }
+
+protected:
+    // runs just after glfwInit() and before the window is created
+    // usefull to give advanced window hints to glfw.
+    virtual bool pre_init() { return true; }
+
+    // runs after every other initialization
+    // opengl functions can be called.
+    virtual bool post_init() { return true; }
+
+    // main function of the application
+    virtual void run() = 0;
+
+    virtual void key_event(KeyEvent event) {}
+
+    virtual void mouse_event(MouseEvent event) {}
+
+    virtual void cursor_event(dvec2 pos) {}
+
+    virtual void scroll_event(dvec2 off) {}
+
+    virtual void resize_event(ivec2 size) {}
+
+private:
+    static void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+    {
+        GLFWApplication* this_ptr = static_cast<GLFWApplication*>(glfwGetWindowUserPointer(window));
+        auto event = KeyEvent { static_cast<Key>(key), static_cast<KeyAction>(action), static_cast<Modifier>(mods) };
+        this_ptr->key_event(event);
+    }
+
+    static void glfw_scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+    {
+        GLFWApplication* this_ptr = static_cast<GLFWApplication*>(glfwGetWindowUserPointer(window));
+        this_ptr->scroll_event(dvec2 { xoffset, yoffset });
+    }
+
+    static void glfw_cursor_callback(GLFWwindow* window, double xpos, double ypos)
+    {
+        GLFWApplication* this_ptr = static_cast<GLFWApplication*>(glfwGetWindowUserPointer(window));
+        this_ptr->cursor_event(dvec2 { xpos, ypos });
+    }
+
+    static void glfw_window_size_callback(GLFWwindow* window, int width, int height)
+    {
+        GLFWApplication* this_ptr = static_cast<GLFWApplication*>(glfwGetWindowUserPointer(window));
+        this_ptr->resize_event(ivec2 { width, height });
+    }
+
+    static void glfw_mouse_button_callback(GLFWwindow* window, int button, int action, int mods) 
+    {
+        GLFWApplication* this_ptr = static_cast<GLFWApplication*>(glfwGetWindowUserPointer(window));
+        MouseEvent event = MouseEvent { static_cast<MouseButton>(button), static_cast<MouseAction>(action), static_cast<Modifier>(mods) };
+        this_ptr->mouse_event(event);
+    }
+
+    static void glfw_error_callback(int error, const char* description)
+    {
+        fprintf(stderr, "GLFW Error: %s\n", description);
+    }
+
+    static APIENTRY void gl_error_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
+        const GLchar* message, const void* this_ptr)
+    {
+        fprintf(stderr, "OpenGL Info: %s\n", message);
+    }
+
+protected:
+    std::string m_name { "GLFW App" };
+    GLFWwindow* m_window { nullptr };
+};
+
+} // namespace mygl
---
+++ inc/MyGL.hpp
@@ -0,0 +1,414 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+namespace mygl {
+using namespace glm;
+
+template<typename T> struct GLTypeTrait {
+};
+
+template<> struct GLTypeTrait<float> {
+    static constexpr GLenum gl_type = GL_FLOAT;
+    static void uniform(int location, float v) { glUniform1f(location, v); }
+    static void uniform_array(int location, size_t count, const float* v) { glUniform1fv(location, count, v); }
+};
+
+template<> struct GLTypeTrait<vec2> {
+    static void uniform(int location, vec2 v) { glUniform2f(location, v.x, v.y); }
+    static void uniform_array(int location, size_t count, const vec2* v)
+    {
+        glUniform2fv(location, count, (const float*)v);
+    }
+};
+
+template<> struct GLTypeTrait<vec3> {
+    static void uniform(int location, vec3 v) { glUniform3f(location, v.x, v.y, v.z); }
+    static void uniform_array(int location, size_t count, const vec3* v)
+    {
+        glUniform3fv(location, count, (const float*)v);
+    }
+};
+
+template<> struct GLTypeTrait<vec4> {
+    static void uniform(int location, vec4 v) { glUniform4f(location, v.x, v.y, v.z, v.w); }
+    static void uniform_array(int location, size_t count, const vec4* v)
+    {
+        glUniform4fv(location, count, (const float*)v);
+    }
+};
+
+template<> struct GLTypeTrait<double> {
+    static constexpr GLenum gl_type = GL_DOUBLE;
+    static void uniform(int location, double v) { glUniform1d(location, v); }
+    static void uniform_array(int location, size_t count, const double* v) { glUniform1dv(location, count, v); }
+};
+
+template<> struct GLTypeTrait<dvec2> {
+    static void uniform(int location, dvec2 v) { glUniform2d(location, v.x, v.y); }
+    static void uniform_array(int location, size_t count, const dvec2* v)
+    {
+        glUniform2dv(location, count, (const double*)v);
+    }
+};
+
+template<> struct GLTypeTrait<dvec3> {
+    static void uniform(int location, dvec3 v) { glUniform3d(location, v.x, v.y, v.z); }
+    static void uniform_array(int location, size_t count, const dvec3* v)
+    {
+        glUniform3dv(location, count, (const double*)v);
+    }
+};
+
+template<> struct GLTypeTrait<dvec4> {
+    static void uniform(int location, dvec4 v) { glUniform4d(location, v.x, v.y, v.z, v.w); }
+    static void uniform_array(int location, size_t count, const dvec4* v)
+    {
+        glUniform4dv(location, count, (const double*)v);
+    }
+};
+
+template<> struct GLTypeTrait<int> {
+    static constexpr GLenum gl_type = GL_INT;
+    static void uniform(int location, int v) { glUniform1i(location, v); }
+    static void uniform_array(int location, size_t count, const int* v) { glUniform1iv(location, count, v); }
+};
+
+template<> struct GLTypeTrait<ivec2> {
+    static void uniform(int location, ivec2 v) { glUniform2i(location, v.x, v.y); }
+    static void uniform_array(int location, size_t count, const ivec2* v)
+    {
+        glUniform2iv(location, count, (const int*)v);
+    }
+};
+
+template<> struct GLTypeTrait<ivec3> {
+    static void uniform(int location, ivec3 v) { glUniform3i(location, v.x, v.y, v.z); }
+    static void uniform_array(int location, size_t count, const ivec3* v)
+    {
+        glUniform3iv(location, count, (const int*)v);
+    }
+};
+
+template<> struct GLTypeTrait<ivec4> {
+    static void uniform(int location, ivec4 v) { glUniform4i(location, v.x, v.y, v.z, v.w); }
+    static void uniform_array(int location, size_t count, const ivec4* v)
+    {
+        glUniform4iv(location, count, (const int*)v);
+    }
+};
+
+template<> struct GLTypeTrait<unsigned int> {
+    static constexpr GLenum gl_type = GL_UNSIGNED_INT;
+    static void uniform(int location, unsigned int v) { glUniform1ui(location, v); }
+    static void uniform_array(int location, size_t count, const unsigned int* v) { glUniform1uiv(location, count, v); }
+};
+
+template<> struct GLTypeTrait<uvec2> {
+    static void uniform(int location, uvec2 v) { glUniform2ui(location, v.x, v.y); }
+    static void uniform_array(int location, size_t count, const uvec2* v)
+    {
+        glUniform2uiv(location, count, (const unsigned int*)v);
+    }
+};
+
+template<> struct GLTypeTrait<uvec3> {
+    static void uniform(int location, uvec3 v) { glUniform3ui(location, v.x, v.y, v.z); }
+    static void uniform_array(int location, size_t count, const uvec3* v)
+    {
+        glUniform3uiv(location, count, (const unsigned int*)v);
+    }
+};
+
+template<> struct GLTypeTrait<uvec4> {
+    static void uniform(int location, uvec4 v) { glUniform4ui(location, v.x, v.y, v.z, v.w); }
+    static void uniform_array(int location, size_t count, const uvec4* v)
+    {
+        glUniform4uiv(location, count, (const unsigned int*)v);
+    }
+};
+
+class ShaderBuilder;
+
+class VertexBuffer {
+
+public:
+    VertexBuffer() { glGenBuffers(1, &m_id); }
+
+    VertexBuffer(const void* data, size_t size)
+    {
+        glGenBuffers(1, &m_id);
+        set_data(data, size);
+    }
+
+    void set_data(const void* data, size_t size)
+    {
+        bind();
+        glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+    }
+
+    void bind() const { glBindBuffer(GL_ARRAY_BUFFER, m_id); }
+
+    void unbind() const { glBindBuffer(GL_ARRAY_BUFFER, 0); }
+
+private:
+    uint32_t m_id;
+};
+
+struct VertexBufferElement {
+    uint32_t count;
+    uint32_t type;
+    uint32_t total_size_in_bytes;
+    uint32_t normalized;
+};
+
+class VertexLayout {
+
+public:
+    VertexLayout() = default;
+
+    template<typename T> void push(uint32_t count, bool normalized = false)
+    {
+        using TraitType = GLTypeTrait<T>;
+
+        m_elements.push_back(
+            VertexBufferElement { count, TraitType::gl_type, (uint32_t)sizeof(T) * count, normalized });
+        m_stride += sizeof(T) * count;
+    }
+
+    uint32_t stride() const { return m_stride; }
+
+    const std::vector<VertexBufferElement>& elements() const { return m_elements; }
+
+private:
+    uint32_t m_stride { 0 };
+    std::vector<VertexBufferElement> m_elements;
+};
+
+class VertexArray {
+
+public:
+    VertexArray() { glGenVertexArrays(1, &m_id); }
+
+    ~VertexArray() { glDeleteVertexArrays(1, &m_id); }
+
+    void bind() const { glBindVertexArray(m_id); }
+
+    void unbind() const { glBindVertexArray(0); }
+
+    void add_buffer(const VertexBuffer& buffer, const VertexLayout& layout)
+    {
+        bind();
+        buffer.bind();
+
+        const auto& elements = layout.elements();
+
+        uint32_t offset = 0;
+
+        for (size_t i = 0; i < elements.size(); i++) {
+            const auto& elem = elements[i];
+
+            glEnableVertexAttribArray(i);
+            glVertexAttribPointer(i, elem.count, elem.type, elem.normalized, layout.stride(), (const void*)offset);
+
+            offset += elem.total_size_in_bytes;
+        }
+    }
+
+private:
+    uint32_t m_id { 0 };
+};
+
+class Shader {
+    friend class ShaderBuilder;
+
+public:
+    void bind() const { glUseProgram(m_id); }
+
+    void unbind() const { glUseProgram(0); }
+
+    template<typename T> void set_uniform(const std::string& name, T value)
+    {
+        using TraitType = GLTypeTrait<T>;
+
+        int loc = get_uniform_location(name);
+        TraitType::uniform(loc, value);
+    }
+
+    template<typename T> void set_uniform_array(const std::string& name, size_t count, const T* values)
+    {
+        using TraitType = GLTypeTrait<T>;
+
+        int loc = get_uniform_location(name);
+        TraitType::uniform_array(loc, count, values);
+    }
+
+    int get_uniform_location(const std::string& name) const
+    {
+        if (auto it = uniform_chache.find(name); it != uniform_chache.end()) {
+            return (*it).second;
+        }
+
+        int loc;
+        loc = glGetUniformLocation(m_id, name.c_str());
+
+        // TODO: maybe do something diffrent if -1 is returnd?
+
+        uniform_chache[name] = loc;
+        return loc;
+    }
+
+private:
+    Shader(uint32_t id) { m_id = id; }
+
+public:
+    uint32_t m_id { 0 };
+    mutable std::unordered_map<std::string, int> uniform_chache;
+};
+
+class ShaderBuilder {
+
+public:
+    ShaderBuilder() = default;
+    ~ShaderBuilder() = default;
+
+    bool add_shader(uint32_t shader_type, std::string path)
+    {
+        assert(
+            shader_type == GL_VERTEX_SHADER || shader_type == GL_GEOMETRY_SHADER || shader_type == GL_FRAGMENT_SHADER);
+
+        auto* file = fopen(path.c_str(), "r");
+
+        if (!file) {
+            perror(path.c_str());
+            return false;
+        }
+
+        fseek(file, 0, SEEK_END);
+        size_t length = ftell(file);
+        fseek(file, 0, SEEK_SET);
+
+        std::string contents;
+        // TODO: what if resize throws ????
+        contents.resize(length);
+
+        size_t num_read = fread(&contents[0], sizeof(char), length, file);
+
+        fclose(file);
+
+        if (num_read != length) {
+            perror(path.c_str());
+            return false;
+        }
+
+        uint32_t shader_id = glCreateShader(shader_type);
+        m_shader_ids[shader_type] = shader_id;
+
+        const char* str = contents.c_str();
+        int size = static_cast<int>(length);
+        glShaderSource(shader_id, 1, &str, &size);
+
+        return true;
+    }
+
+    bool compile_and_link()
+    {
+        m_program_id = glCreateProgram();
+
+        for (auto pair : m_shader_ids) {
+            auto shader_id = pair.second;
+            auto shader_type = pair.first;
+            glCompileShader(shader_id);
+            glAttachShader(m_program_id, shader_id);
+        }
+
+        glLinkProgram(m_program_id);
+        glValidateProgram(m_program_id);
+
+        for (auto pair : m_shader_ids) {
+            auto shader_id = pair.second;
+            glDetachShader(m_program_id, shader_id);
+            glDeleteShader(shader_id);
+        }
+
+        return true; // TODO: compile and link error checks
+    }
+
+    Shader finish() { return Shader(m_program_id); }
+
+private:
+    bool check_compile_error(uint32_t shader_id, uint32_t shader_type)
+    {
+        int result;
+        glGetShaderiv(shader_id, GL_COMPILE_STATUS, &result);
+
+        if (result == GL_FALSE) {
+            int len;
+            glGetShaderiv(shader_id, GL_INFO_LOG_LENGTH, &len);
+
+            std::string msg;
+            msg.resize(len);
+            glGetShaderInfoLog(shader_id, len, &len, &msg[0]);
+
+            std::string shader_name;
+            switch (shader_type) {
+            case GL_VERTEX_SHADER:
+                shader_name = "vertex shader";
+                break;
+            case GL_GEOMETRY_SHADER:
+                shader_name = "geometry shader";
+                break;
+            case GL_FRAGMENT_SHADER:
+                shader_name = "fragment shader";
+                break;
+            default:
+                shader_name = "unknown shader";
+                break;
+            }
+
+            fprintf(stderr, "Failed to compile %s: %s\n", shader_name.c_str(), msg.c_str());
+
+            return false;
+        }
+
+        return true;
+    }
+
+    bool check_link_error()
+    {
+        int result;
+        glGetProgramiv(m_program_id, GL_LINK_STATUS, &result);
+
+        if (result == GL_FALSE) {
+            int len;
+            glGetProgramiv(m_program_id, GL_INFO_LOG_LENGTH, &len);
+
+            std::string msg;
+            msg.resize(len);
+
+            glGetProgramInfoLog(m_program_id, len, &len, &msg[0]);
+
+            fprintf(stderr, "Shader failed to link: %s\n", msg.c_str());
+
+            return false;
+        }
+
+        return true;
+    }
+
+private:
+    uint32_t m_program_id;
+    std::unordered_map<uint32_t, uint32_t> m_shader_ids;
+};
+
+}
